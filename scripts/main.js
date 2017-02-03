0464f3d1f819e58f62dc08f962be94df101de0af
Christopher Mitchell <mrmperial@MrMPerial.local>
2017-02-03 15:53:40 -0600

    Added Main and C2A

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,29 @@
 $(document).ready(function () {
+
+    $('.com').hide();
+    $('.spec').hide();
+
     $('#menuButton').on('click', function () {
         $('#menu').slideToggle(1000);
     });
+
+    $('#resButton').on('click', function () {
+        $('.res').show(1000);
+        $('.com').hide(1000);
+        $('.spec').hide(1000);
+    });
+
+    $('#comButton').on('click', function () {
+        $('.com').show(1000);
+        $('.res').hide(1000);
+        $('.spec').hide(1000);
+    });
+
+    $('#specButton').on('click', function () {
+        $('.spec').show(1000);
+        $('.res').hide(1000);
+        $('.com').hide(1000);
+    });
+
+
 });
